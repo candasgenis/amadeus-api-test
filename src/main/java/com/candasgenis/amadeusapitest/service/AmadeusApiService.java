3ad0deaab87eda4f83d27a4bbac5e631a3744966
Candas Genis cwx1322782 <candas.genis3@huawei-partners.com>
2024-05-04 15:25:32 +0300

    Exception of getLocations updated

--- src/main/java/com/candasgenis/amadeusapitest/service/AmadeusApiService.java
+++ src/main/java/com/candasgenis/amadeusapitest/service/AmadeusApiService.java
@@ -1,7 +1,8 @@
 package com.candasgenis.amadeusapitest.service;
 
+import com.amadeus.exceptions.ResponseException;
 import com.google.gson.JsonObject;
 
 public interface AmadeusApiService {
-    JsonObject getLocations(String keyword, String subType) throws Exception;
+    JsonObject getLocations(String keyword, String subType) throws ResponseException;
 }
